1be5d711551439b301e41a975beab2ca3efde5bb
swizz <emailswizz@gmail.com>
2024-02-10 17:53:37 -0500

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import { logData } from '../utils/mongodb'; 
+import { logData } from '../utils/mongodb';
 import {
   FrameButton,
   FrameContainer,
   FrameImage,
-  FrameInput,
   FrameReducer,
   NextServerPageProps,
   getPreviousFrame,
@@ -12,89 +11,64 @@ import {
 } from "frames.js/next/server";
 import Link from "next/link";
 import { DEBUG_HUB_OPTIONS } from "./debug/constants";
-import { getTokenUrl } from "frames.js";
 
 type State = {
   active: string;
   total_button_presses: number;
 };
 
-const initialState = { active: "1", total_button_presses: 0 };
+const initialState: State = { active: "1", total_button_presses: 0 };
 
 const reducer: FrameReducer<State> = (state, action) => {
   return {
     total_button_presses: state.total_button_presses + 1,
     active: action.postBody?.untrustedData.buttonIndex
       ? String(action.postBody?.untrustedData.buttonIndex)
-      : "1",
+      : state.active,
   };
 };
 
-// This is a react server component only
-export default async function Home({
-  params,
-  searchParams,
-}: NextServerPageProps) {
+export default function Home({ searchParams }: NextServerPageProps) {
   const previousFrame = getPreviousFrame<State>(searchParams);
-
-  const frameMessage = await getFrameMessage(previousFrame.postBody, {
-    ...DEBUG_HUB_OPTIONS,
-  });
-
-  if (frameMessage) {
-    const logObject = {
-      buttonIndex: frameMessage.buttonIndex,
-      castId: frameMessage.castId,
-      inputText: frameMessage.inputText,
-      requesterFid: frameMessage.requesterFid,
-      isValid: frameMessage.isValid,
-      casterFollowsRequester: frameMessage.casterFollowsRequester,
-      requesterFollowsCaster: frameMessage.requesterFollowsCaster,
-      likedCast: frameMessage.likedCast,
-      recastedCast: frameMessage.recastedCast,
-      requesterVerifiedAddresses: frameMessage.requesterVerifiedAddresses,
-      requesterUserData: frameMessage.requesterUserData,
-      // Add any additional fields you want to log
-    };
-
-    logData("frameMessages", logObject)
-    .then(() => console.log("Data logged to MongoDB"))
-    .catch((err) => console.error("Failed to log data to MongoDB", err));
-}
-
-
   const [state, dispatch] = useFramesReducer<State>(
     reducer,
     initialState,
     previousFrame
   );
 
-  // Here: do a server side side effect either sync or async (using await), such as minting an NFT if you want.
-  // example: load the users credentials & check they have an NFT
+  const frameMessagePromise = getFrameMessage(previousFrame.postBody, {
+    ...DEBUG_HUB_OPTIONS,
+  });
 
-  console.log("info: state is:", state);
+  frameMessagePromise.then((frameMessage) => {
+    if (frameMessage) {
+      const logObject = {
+        buttonIndex: frameMessage.buttonIndex,
+        castId: frameMessage.castId,
+        inputText: frameMessage.inputText,
+        requesterFid: frameMessage.requesterFid,
+        isValid: frameMessage.isValid,
+        casterFollowsRequester: frameMessage.casterFollowsRequester,
+        requesterFollowsCaster: frameMessage.requesterFollowsCaster,
+        likedCast: frameMessage.likedCast,
+        recastedCast: frameMessage.recastedCast,
+        requesterVerifiedAddresses: frameMessage.requesterVerifiedAddresses,
+        requesterUserData: frameMessage.requesterUserData,
+      };
 
-  if (frameMessage) {
-    const {
-      isValid,
-      buttonIndex,
-      inputText,
-      castId,
-      requesterFid,
-      casterFollowsRequester,
-      requesterFollowsCaster,
-      likedCast,
-      recastedCast,
-      requesterVerifiedAddresses,
-      requesterUserData,
-    } = frameMessage;
-
-    console.log("info: frameMessage is:", frameMessage);
-  }
+      logData("frameMessages", logObject)
+      .then(() => console.log("Data logged to MongoDB"))
+      .catch((err) => console.error("Failed to log data to MongoDB", err));
+    }
+  });
 
   const baseUrl = process.env.NEXT_PUBLIC_HOST || "http://localhost:3000";
 
-  // then, when done, return next frame
+  // Function to handle button click
+  const handleButtonClick = (teamName: string) => {
+    dispatch({ type: 'action', postBody: { untrustedData: { team: teamName } } });
+  };
+
   return (
     <div className="p-4">
       frames.js starter kit. The Template Frame is on this page, it's in
@@ -106,26 +80,14 @@ export default async function Home({
         state={state}
         previousFrame={previousFrame}
       >
-        {/* FrameImage to provide visual context or branding */}
-        <FrameImage>
-          <div style={{ width: '100%', height: '100%', backgroundColor: 'slategray', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            {frameMessage?.inputText ? frameMessage.inputText : "Super Bowl winner. Like/RC/Follow to boost entry. Unlimited entries"}
-          </div>
-        </FrameImage>
-  
-    
-  
-        {/* FrameButton for "49ers" */}
-        <FrameButton onClick={() => dispatch({ type: 'action', postBody: { untrustedData: { team: "49ers" } } })}>
+        <FrameImage src="https://picsum.photos/seed/frames.js/1146/600" />
+        <FrameButton action="post" onClick={() => handleButtonClick("49ers")}>
           49ers
         </FrameButton>
-  
-        {/* FrameButton for "Chiefs" */}
-        <FrameButton onClick={() => dispatch({ type: 'action', postBody: { untrustedData: { team: "Chiefs" } } })}>
+        <FrameButton action="post" onClick={() => handleButtonClick("Chiefs")}>
           Chiefs
         </FrameButton>
       </FrameContainer>
     </div>
   );
-  
 }
